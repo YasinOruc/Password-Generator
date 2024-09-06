62c0b331e1f39a04a1494b43b29e79d7ac8085f9
YasinOruc <orucyasin14@gmail.com>
2024-09-06 19:08:57 +0200

    function generatepassword

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(){
     var button = document.querySelector('.button');
 
-    button.addEventListener('click',function(){
+    button.addEventListener('click', function(){
         var passwordDiv = document.querySelector('.password-text');
-        passwordDiv.textContent = 'new password';
-    })
-})
+        var password = generatePassword();
+        passwordDiv.textContent = password;
+    });
+});
+
+function generatePassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+    for (var i = 0; i < passwordLength; i++){
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    return password;
+}
