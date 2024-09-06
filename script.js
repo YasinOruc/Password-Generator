d5ca8e379fb10cdf2cf875be592b733fd33d85fa
YasinOruc <orucyasin14@gmail.com>
2024-09-06 19:29:17 +0200

    optimized code

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     var button = document.querySelector('.button');
+    var copyImg = document.querySelector('.copy');
+    var passwordDiv = document.querySelector('.password-text');
 
-    button.addEventListener('click', function(){
-        var passwordDiv = document.querySelector('.password-text');
+    button.addEventListener('click', function () {
         var password = generatePassword();
         passwordDiv.textContent = password;
+        passwordDiv.appendChild(copyImg);
+        copyImg.style.display = 'block';
+    });
+
+    copyImg.addEventListener('click', function () {
+        var password = passwordDiv.textContent;
+        navigator.clipboard.writeText(password)
+        .then(() => {
+            alert('Password copied to clipboard!');
+        });
     });
 });
 
@@ -12,9 +23,9 @@ function generatePassword() {
     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var passwordLength = 12;
     var password = "";
-    for (var i = 0; i < passwordLength; i++){
+    for (var i = 0; i < passwordLength; i++) {
         var randomNumber = Math.floor(Math.random() * chars.length);
-        password += chars.substring(randomNumber, randomNumber + 1);
+        password += chars.charAt(randomNumber);  
     }
     return password;
 }
